d2e3d160bcdbb8a30bd91decea25a3cbc64f1c1c
Ethan <ethan.durham3692@gmail.com>
2017-05-15 11:26:42 -0700

    Updating GoLive component
    Signed-off-by: Ethan <ethan.durham3692@gmail.com>

--- angular-src/src/app/components/go-live/go-live.component.ts
+++ angular-src/src/app/components/go-live/go-live.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { OrdersModel } from '../../models/orders.model';
 
 @Component({
   selector: 'veer-go-live',
@@ -6,11 +7,59 @@ import { Component } from '@angular/core';
   styleUrls: ['./go-live.component.css']
 })
 
-export class GoLiveComponent {
+export class GoLiveComponent implements OnInit {
   public headerMessagePartOne = "Welcome to ";
   public headerMessagePartTwo = "Chloe's Lemonade";
   public subHeaderMessage = "Make a selection below and register for future notifications and alerts";
   public lemonadePrice = 2;
   public strawberryLemonadePrice = 2.5;
   public homemadeCookiesPrice = 0.75;
+  public addedToCart: Boolean;
+  public ordersModel: OrdersModel = new OrdersModel();
+
+  ngOnInit() {
+    this.clearTotals();
+    this.addedToCart = false;
+  }
+
+  lemonadeAddToCart() {
+    this.addedToCart = true;
+    this.ordersModel.lemonadeQuantity = (this.ordersModel.lemonadeQuantity + 1);
+    this.ordersModel.lemonadeTotal = (this.ordersModel.lemonadeQuantity * this.lemonadePrice);
+    this.ordersModel.subTotal = (this.ordersModel.lemonadeTotal + this.ordersModel.cookieTotal + this.ordersModel.strawberryTotal);
+    this.ordersModel.itemsAdded = (this.ordersModel.lemonadeQuantity + this.ordersModel.cookieQuantity + this.ordersModel.strawberryQuantity);
+    console.log('Lemonade Quantity and Total: ' + this.ordersModel.lemonadeQuantity + ' & ' + this.ordersModel.lemonadeTotal);
+    console.log('Subtotal = ' + this.ordersModel.subTotal);
+  }
+
+  strawberryAddToCart() {
+    this.addedToCart = true;
+    this.ordersModel.strawberryQuantity = (this.ordersModel.strawberryQuantity + 1);
+    this.ordersModel.strawberryTotal = (this.ordersModel.strawberryQuantity * this.strawberryLemonadePrice);
+    this.ordersModel.subTotal = (this.ordersModel.lemonadeTotal + this.ordersModel.cookieTotal + this.ordersModel.strawberryTotal);
+    this.ordersModel.itemsAdded = (this.ordersModel.lemonadeQuantity + this.ordersModel.cookieQuantity + this.ordersModel.strawberryQuantity);
+    console.log('Strawberry Quantity and Total: ' + this.ordersModel.strawberryQuantity + ' & ' + this.ordersModel.strawberryTotal);
+    console.log('Subtotal = ' + this.ordersModel.subTotal);
+  }
+
+  cookieAddToCart() {
+    this.addedToCart = true;
+    this.ordersModel.cookieQuantity = (this.ordersModel.cookieQuantity + 1);
+    this.ordersModel.cookieTotal = (this.ordersModel.cookieQuantity * this.homemadeCookiesPrice);
+    this.ordersModel.subTotal = (this.ordersModel.lemonadeTotal + this.ordersModel.cookieTotal + this.ordersModel.strawberryTotal);
+    this.ordersModel.itemsAdded = (this.ordersModel.lemonadeQuantity + this.ordersModel.cookieQuantity + this.ordersModel.strawberryQuantity);
+    console.log('Cookie Quantity and Total: ' + this.ordersModel.cookieQuantity + ' & ' + this.ordersModel.cookieTotal);
+    console.log('Subtotal = ' + this.ordersModel.subTotal);
+  }
+
+  clearTotals() {
+    this.ordersModel.lemonadeQuantity = 0;
+    this.ordersModel.lemonadeTotal = 0;
+    this.ordersModel.strawberryQuantity = 0;
+    this.ordersModel.strawberryTotal = 0;
+    this.ordersModel.cookieQuantity = 0;
+    this.ordersModel.cookieTotal = 0;
+    this.ordersModel.itemsAdded = 0;
+    this.ordersModel.subTotal = 0;
+  }
 }
---
+++ angular-src/src/app/models/orders.model.ts
@@ -0,0 +1,10 @@
+export class OrdersModel {
+  lemonadeQuantity: any;
+  lemonadeTotal: any;
+  strawberryQuantity: any;
+  strawberryTotal: any;
+  cookieQuantity: any;
+  cookieTotal: any;
+  itemsAdded: any;
+  subTotal: any;
+}
